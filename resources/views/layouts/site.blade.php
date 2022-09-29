d6355d35cf51ec623ba588431344c032557a8b75
tiarman <tiarmancr@gmail.com>
2022-09-29 23:31:35 +0600

    updated

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>a promise</title>
+    <title>sea alarm</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
@@ -44,34 +44,34 @@
 <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center">
 
-        {{-- <h1 class="logo me-auto"><a href="{{route('home')}}">A-Promise</a></h1> --}}
+         <h1 class="logo me-auto"><a href="">SEA ALARM</a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html" class="logo me-auto"><img src="assets/frontend/img/logo.png" alt="" class="img-fluid"></a>-->
 
         <nav id="navbar" class="navbar">
             <ul>
-                {{-- <li><a class="nav-link scrollto active" href="{{route('home')}}">Home</a></li> --}}
-                <li><a class="nav-link scrollto" href="#about">About</a></li>
-                <li><a class="nav-link scrollto" href="#services">Services</a></li>
-                <li><a class="nav-link   scrollto" href="#portfolio">Portfolio</a></li>
-                <li><a class="nav-link scrollto" href="#team">Team</a></li>
-                <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-                    <ul>
-                        <li><a href="#">Drop Down 1</a></li>
-                        <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                            <ul>
-                                <li><a href="#">Deep Drop Down 1</a></li>
-                                <li><a href="#">Deep Drop Down 2</a></li>
-                                <li><a href="#">Deep Drop Down 3</a></li>
-                                <li><a href="#">Deep Drop Down 4</a></li>
-                                <li><a href="#">Deep Drop Down 5</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Drop Down 2</a></li>
-                        <li><a href="#">Drop Down 3</a></li>
-                        <li><a href="#">Drop Down 4</a></li>
-                    </ul>
-                </li>
+                 <li><a class="nav-link scrollto active" href="">Home</a></li>
+                <li><a class="nav-link scrollto" href="#about">Routine</a></li>
+                <li><a class="nav-link scrollto" href="#services">My Check List</a></li>
+{{--                <li><a class="nav-link   scrollto" href="#portfolio">Portfolio</a></li>--}}
+{{--                <li><a class="nav-link scrollto" href="#team">Team</a></li>--}}
+{{--                <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>--}}
+{{--                    <ul>--}}
+{{--                        <li><a href="#">Drop Down 1</a></li>--}}
+{{--                        <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>--}}
+{{--                            <ul>--}}
+{{--                                <li><a href="#">Deep Drop Down 1</a></li>--}}
+{{--                                <li><a href="#">Deep Drop Down 2</a></li>--}}
+{{--                                <li><a href="#">Deep Drop Down 3</a></li>--}}
+{{--                                <li><a href="#">Deep Drop Down 4</a></li>--}}
+{{--                                <li><a href="#">Deep Drop Down 5</a></li>--}}
+{{--                            </ul>--}}
+{{--                        </li>--}}
+{{--                        <li><a href="#">Drop Down 2</a></li>--}}
+{{--                        <li><a href="#">Drop Down 3</a></li>--}}
+{{--                        <li><a href="#">Drop Down 4</a></li>--}}
+{{--                    </ul>--}}
+{{--                </li>--}}
                 <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
                 <li><a class="getstarted scrollto" href="{{route('login')}}">Login</a></li>
             </ul>
